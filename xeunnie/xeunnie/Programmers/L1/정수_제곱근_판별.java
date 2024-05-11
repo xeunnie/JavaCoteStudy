745bac812f850474b1c6e2d8526cc72309155971
chloe_choi <plumxeun@gmail.com>
2024-05-11 23:19:57 +0900

    add: 정수 제곱근 판별.java

---
+++ xeunnie/xeunnie/Programmers/L1/정수_제곱근_판별.java
@@ -0,0 +1,8 @@
+class Solution {
+    public long solution(long n) {
+        int x = (int)Math.sqrt(n);
+        if (n == (long)x*x){
+            return (long)(x+1)*(x+1);
+        } else return -1;
+    }
+}
