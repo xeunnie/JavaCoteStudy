52e6580f5a539a1d2b3936cae84c851cca76b245
chloe_choi <plumxeun@gmail.com>
2024-05-13 17:41:40 +0900

    add: 두 정수 사이의 합.java

--- xeunnie/xeunnie/Programmers/L1/정수_제곱근_판별.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public long solution(long n) {
-        int x = (int)Math.sqrt(n);
-        if (n == (long)x*x){
-            return (long)(x+1)*(x+1);
-        } else return -1;
-    }
-}
