200c3c937295a080c66b79e6e0695923a469ac66
SihyunSeo <tjtlgus4@naver.com>
2024-05-16 19:52:44 +0900

    add: 멀리 뛰기.java

---
+++ ssh/Programmers/L2/멀리 뛰기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long solution(int n) {
+        long[] answer = new long[n+1];
+
+        answer[0] = 1;
+        answer[1] = 2;
+
+        for(int i = 2; i < n; i++) {
+            answer[i] = (answer[i-1] + answer[i-2]) % 1234567;
+        }
+
+        return answer[n-1];
+    }
+}
