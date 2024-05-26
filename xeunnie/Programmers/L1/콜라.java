59b1322d87c80775a72e482c667b614865749f0d
chloe_choi <plumxeun@gmail.com>
2024-05-27 01:48:57 +0900

    add: 콜라.java

---
+++ xeunnie/Programmers/L1/콜라.java
@@ -0,0 +1,14 @@
+package xeunnie.Programmers.L1;
+
+public class 콜라 {
+    public static int solution(int a, int b, int n) {
+        int total_colas = 0;
+
+        while (n >= a) {
+            int new_colas = (n / a) * b;
+            total_colas += new_colas;
+            n = (n % a) + new_colas;
+        }
+        return total_colas;
+    }
+}
