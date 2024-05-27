06190af4297710b9194039f266da28983f1e73e0
706com <ehdgusjdbswl@gmail.com>
2024-05-27 20:52:52 +0900

    add: 모음사전.java

---
+++ 706com/programmers/L2/모음사전.java
@@ -0,0 +1,51 @@
+//[240627] : 20분
+
+import java.util.*;
+
+class Solution {
+    static int N;
+    static String[] arr;
+    static String[] output;
+    static int count = 0;
+    static boolean end = false;
+    public int solution(String word) {
+        int answer = 0;
+        N = 5;
+        int len = word.length();
+        arr = new String[]{"A","E","I","O","U"};
+        output = new String[5];
+
+        dfs(0, 0,len,word);
+        return count -1;
+    }
+
+    public void dfs(int start, int depth, int len,String word){
+        if(end){
+            return;
+        }
+        count++;
+        if(depth == len){
+            String str = "";
+            for(int i=0; i<depth; i++){
+                str += output[i];
+            }
+            // System.out.println(str+" : "+count);
+            if(str.equals(word)){
+                end = true;
+                return;
+            }
+        }
+        if(depth == N){
+            // System.out.println(Arrays.toString(output)+" : "+count);
+            return;
+        }
+
+        for(int i=0; i<N; i++){
+            output[depth] = arr[i];
+            dfs(i,depth+1,len,word);
+            if(end){
+                return;
+            }
+        }
+    }
+}
