e696356156f81577aca7a070d6972bd453e4964f
chloe_choi <plumxeun@gmail.com>
2024-06-03 19:01:20 +0900

    add: 나머지가 1이 되는 수 찾기.java

---
+++ xeunnie/Programmers/L1/나머지가_1이_되는_수_찾기.java
@@ -0,0 +1,13 @@
+package xeunnie.Programmers.L1;
+
+class Solution {
+    public int solution(int n) {
+            
+        for (int i=1; i<n; i++){
+            if(n%i == 1){
+                return i;
+            }
+        }
+        return n;
+    }
+}
